343964cdf26bf7b41a660213412784a9b9375769
DanielSilverio <daniel.silverio@metasix.com.br>
2017-07-24 11:53:55 -0300

    Iniciado o frontend

--- src/main/java/br/com/silentlight/cronos/controller/CapituloController.java
+++ src/main/java/br/com/silentlight/cronos/controller/CapituloController.java
@@ -1,13 +1,16 @@
 package br.com.silentlight.cronos.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.WebMvcProperties.View;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.silentlight.cronos.entity.Capitulo;
 import br.com.silentlight.cronos.service.CapituloService;
@@ -25,8 +28,20 @@ public class CapituloController {
 	}
 	
 	@GetMapping
-	public List<Capitulo> findAll(){
-		return service.findAll();
+	public ModelAndView findAll(){
+		ModelAndView mv = new ModelAndView("views/capitulos");
+		mv.addObject("capitulos", service.findAll());
+		mv.addObject("capitulo", new Capitulo());
+		return mv;
+		/*
+		List<String> teste = new ArrayList<>();
+		teste.add("aaa");
+		teste.add("bbb");
+		
+		mv.addObject("capitulos", teste);
+		
+		return mv;
+		*/
 	}
 	
 	@GetMapping("/{numero}")
@@ -35,8 +50,9 @@ public class CapituloController {
 	}
 	
 	@PostMapping
-	public void save(Capitulo capitulo){
+	public ModelAndView save(Capitulo capitulo){
 		service.save(capitulo);
+		return findAll();
 	}
 
 }
