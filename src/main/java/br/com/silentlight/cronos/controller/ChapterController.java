0be20380a255805f758ff7f2d9225424aeff6150
DanielSilverio <daniel.silverio@metasix.com.br>
2017-08-02 11:55:31 -0300

    resolucao de problemas no uso da api, mas array chega vazio no templatecd

--- src/main/java/br/com/silentlight/cronos/controller/ChapterController.java
+++ src/main/java/br/com/silentlight/cronos/controller/ChapterController.java
@@ -3,12 +3,12 @@ package br.com.silentlight.cronos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import br.com.silentlight.cronos.entity.Chapter;
 import br.com.silentlight.cronos.service.ChapterService;
@@ -20,17 +20,21 @@ public class ChapterController {
 	@Autowired
 	private ChapterService service;
 	
-	@GetMapping
+	@GetMapping(produces="application/json")
+	//@RequestMapping(method = RequestMethod.GET, produces="application/json")
+	@CrossOrigin(origins="*")
 	public List<Chapter> findAll(){
 		return service.findAll();
 	}
 	
 	@GetMapping("/{number}")
+	@CrossOrigin(origins="*")
 	public Chapter findByNumero(@PathVariable(name="number") long number){
 		return service.findOne(number);
 	}
 	
 	@PostMapping
+	@CrossOrigin(origins="*")
 	public void save(Chapter chapter){
 		service.save(chapter);
 	}
