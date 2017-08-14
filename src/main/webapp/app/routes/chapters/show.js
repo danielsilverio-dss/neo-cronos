8d397b03673366bcb569ce72e18db8013557ddf2
Daniel <dss.123@hotmail.com>
2017-08-13 22:48:30 -0300

    Melhorias importantes no layout e parte incompleta da funcionalidade de salvar capitulo

--- src/main/webapp/app/routes/chapters/show.js
+++ src/main/webapp/app/routes/chapters/show.js
@@ -3,11 +3,41 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
 	model(){
-		return this.store.findAll('chapter');
+		return Ember.RSVP.hash({
+			chapters: this.store.findAll('chapter'),
+			//newChapter: this.store.createRecord('chapter')
+		}) 
+		
+		
+		
 	},
 	
 	setupController(controller, model){
-		controller.set('chapters', model);
+		controller.set('chapters', model.chapters);
+		//controller.set('newChapter', model.newChapter)
+	},
+	
+	actions: {
+		saveChapter(number, title, description){
+			//let chapter = model.newChapter;
+			//let chapter = this.get('controller.model.newChapter');
+			
+			console.log('number: ' + number)
+			console.log('title: ' + title)
+			console.log('description: ' + description)
+			
+			let chapter = this.store.createRecord('chapter', {number, title, description})
+			
+			console.log('------------------------------')
+			console.log('number: ' + chapter.number)
+			console.log('title: ' + chapter.title)
+			console.log('description: ' + chapter.description)
+			
+			
+			chapter.save().then(function(){
+				console.log('salvo com sucesso!')
+			}).catch()
+		}
 	}
 	
 });
